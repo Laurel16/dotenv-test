ed4eac74fa0200a309945cfebfe346d442a4fcb0
Laure Leter <laure.leter@gmail.com>
2018-12-06 10:46:54 +0100

    changing sqlite3 fir postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2018_12_06_011020) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.string "image"
